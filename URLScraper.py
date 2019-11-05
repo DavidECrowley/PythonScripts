a0ff39e4dc37a06d1b8e4b03cd3fa5101731cc00
David Crowley <56149778+DavidECrowley@users.noreply.github.com>
2019-11-05 01:29:20 +0000

    Add files via upload
    A simple python script for scraping all URLS related to the TARGET address.

---
+++ URLScraper.py
@@ -0,0 +1,30 @@
+# ScrapeNet - scrapes a desired URL of all links and writes them to a new file
+# will overwrite the same file if a new name is not given
+# Uses BeautifulSoup's Library for scraping pages
+
+def ScrapeNet( filename, urls ) :
+    
+    from bs4 import BeautifulSoup
+    import requests
+    
+    try :
+        filehandle = open(filename,"w")
+    except IOError :
+        print( "*** ScrapeNet : Cannot open the file %s" % ( filename ) )
+        return
+    
+    for url in urls :
+        
+        num_line = 0
+        r  = requests.get( "http://" + url )
+        data = r.text
+        soup = BeautifulSoup( data , features="html5lib")    
+        filehandle.write( "%s" % ( "-" * 50 )+"\n" )
+        filehandle.write( "All Link Scrapes for '%s'\n" % ( url ) + "-" * 50 + "\n\n" )
+        
+        for link in soup.find_all( 'a' ):
+            num_line += 1
+            print( num_line,link.get( 'href' ) )
+            filehandle.write( "%3i - %s" % ( num_line, link.get( 'href' )) + '\n' )
+
+ScrapeNet( "links.txt", [ "www.linkedin.com" ] )
